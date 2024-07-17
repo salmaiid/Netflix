b699b893662735b96cbe613e680e1e490042e994
salmaiid <salmaiid671@gmail.com>
2024-07-17 00:59:36 -0700

    Components

---
+++ Netflix1/src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css'
+import Navbar from "../../Components/Navbar/Navbar"
+
+const Home = () => {
+    return(
+        <div className="Home">
+            <Navbar />
+
+        </div>
+    )
+}
+export default Home

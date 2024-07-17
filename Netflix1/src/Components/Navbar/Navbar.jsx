b7635f66b373869eb62275929769db6fea7a8402
salmaiid <salmaiid671@gmail.com>
2024-07-17 02:33:44 -0700

    assets

--- Netflix1/src/Components/Navbar/Navbar.jsx
+++ Netflix1/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import './Navbar.css'
+import Logo from '../../assets/logo.png';
 
 const Navbar = () => {
     return (
         <div className="Navbar">
-            navbar
+            <div className="navbar-left">
+                <image src="{Logo}" alt ="" />
+            </div>
+            <div className="navbar-right"></div>
 
         </div>
     )
